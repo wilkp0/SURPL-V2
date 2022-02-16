5073b25064f0ec3459155d4ec5288d639a0c1c1d
Jordan <superusersudoroot@gmail.com>
2022-02-16 14:20:20 -0500

    Fixing and working through with Tensorboard

--- JJ_cust/SBEV.py
+++ JJ_cust/SBEV.py
@@ -2,6 +2,8 @@ from gym import Env
 from gym.spaces import Discrete, Box
 from gym.envs.classic_control import rendering
 from datetime import datetime
+import numpy as np
+
 # global golbalTime 
 
 # now = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
@@ -15,26 +17,34 @@ fileOut = open(sourceDir + "/results/PPO.txt", "w+")
 class SmartBuildingEnv(Env):
     def __init__(self):
         self.viewer = None
+        self.load = []
         self.demand = []
-        self.load = 0
+        # self.demandCharge = 2 * self.load
+        self.demanCharge = 0
         self.timeStep = 0
         self.deltaUtilization = 0
         self.penalty = 0
+        self.totalReward = 0
         self.action_space = None
         self.observation_space = None
         self.reset()
         
     def reset(self):
         # self.load = 0
-        # self.demand = [3, 1, 1]
-        self.demand = [3, 2, 1]
-        # self.timeStep +=1 == self.demand[self.timeStep]
-        # self.timeStep = 0
-        self.deltaUtilization = abs(self.demand[self.timeStep] - self.load)
+        # MOVE TO 24 HOURS FOR INITAL RESULTS NOW THAT PRELIM RESULTS ESTABLISHED
+        self.demand = [3, 1, 1]
+        self.load = []
+        self.timeWindow = len(self.demand) - 1
+        self.timeStep = 0
+        self.totalReward = 0
+        # self.deltaUtilization = abs(self.demand[self.timeStep] - self.load[self.timeStep])
+        self.deltaUtilization = 0
         self.penalty = (self.deltaUtilization) ** 2
         self.action_space = Box(low=0, high=self.demand[self.timeStep], shape=(1,), dtype=float)
-        self.observation_space = Box(low=0, high=max([self.deltaUtilization]), shape=(1,), dtype=float)
-        return 0 
+        # OBSERVATION SPACE CAN ALSO INCLUDE DEMAND CHARGE, TIME, ETC -> 1 INSUFFICIENT FOR MORE RESULTS
+        self.observation_space = Box(low=np.array([0, 0]), high=np.array([self.deltaUtilization, self.timeWindow]), shape=(2,), dtype=float)
+        # return np.array(0)
+        return np.array([0, 0])
         # pass
         # return state
     
@@ -42,36 +52,32 @@ class SmartBuildingEnv(Env):
         info = {}
         reward = 0
         done:bool = False
-        self.load = action[0]
+        self.load.append(action[0])
+        self.demandCharge = max(self.load)
 
-        print("Stepping action: ", round(action[0], 2))
+        print("SB stepping: ", round(self.load[self.timeStep], 2))
         print("*"*25)
         print("*"*25)
-        print("Stepping action: ", round(action[0], 2), file=fileOut)
+        print("SB stepping: ", round(self.load[self.timeStep], 2), file=fileOut)
         print("*"*25, file=fileOut)
         print("*"*25, file=fileOut)
         
-        
-        # self.timeStep += 1
-        # while not done:
-        print("A: ", action)
-        print("A: ", action, file=fileOut)
+
         print("Timestep: ", self.timeStep)
         print("-"*5)
-        print("Load: ", round(self.load, 2))
+        print("Load: ", [round(i, 2) for i in self.load][self.timeStep])
         print("Demand: ", self.demand[self.timeStep])
         print("Timestep: ", self.timeStep, file=fileOut)
         print("-"*5, file=fileOut)
-        print("Load: ", round(self.load, 2), file=fileOut)
+        print("Load: ", [round(i, 2) for i in self.load][self.timeStep], file=fileOut)
         print("Demand: ", self.demand[self.timeStep], file=fileOut)
 
-        # print(action)
-        # self.deltaUtilization = 0
-        self.deltaUtilization = abs(self.demand[self.timeStep] - self.load)
+
+        self.deltaUtilization = abs(self.demand[self.timeStep] - self.load[self.timeStep])
         self.penalty = (self.deltaUtilization) ** 2
         
-        # self.load = action[0]
         observation = self.demand[self.timeStep]
+        
         print("Penalty: ", round(self.penalty, 2))
         print("Delta ", round(self.deltaUtilization, 2))
         print("Observation: ", observation)
@@ -81,18 +87,46 @@ class SmartBuildingEnv(Env):
         print("Observation: ", observation, file=fileOut)
         print("-"*25, file=fileOut)
         
-        reward -= self.load + self.penalty
+        # STILL NEED PEAK DEMAND IN REWARD
+        # HOW DO WE CONTRIBUTE TO THIS REWARD?
+               
+        if self.timeStep == self.timeWindow:
+            reward -= self.load[self.timeStep] + self.penalty + 2*(self.demandCharge)
+        # IF HAVEN'T REACHED END OF SIMULATION YET
+        elif self.timeStep < self.timeWindow and self.load[self.timeStep] != 0:
+            reward -= self.load[self.timeStep] + self.penalty
+            
+        else:
+            reward -= self.load[self.timeStep] + 1 + self.penalty
+            
+        # REWARD CAN BE DIFFERENCE BETWEEN COSTS
+
         self.timeStep += 1
+        self.totalReward += reward
         done = True if self.timeStep > 2 else done
+        print("Total Reward: ", round(self.totalReward, 2))
+        print("-"*25)
+        print("Total Reward: ", round(self.totalReward, 2), file=fileOut)
+        print("-"*25, file=fileOut)
+        print("-"*25, file=fileOut)
+        print("-"*25)
+        print("-"*25)
+        if done:
+            print("DONE: \nLoad: ", [round(i, 2) for i in self.load], "\nTotal Reward: ", round(self.totalReward, 2))
+            print("DONE: \nLoad: ", [round(i, 2) for i in self.load], "\nTotal Reward: ", round(self.totalReward, 2), file=fileOut)
+            print("-"*25)
+            print("-"*25)
+            print("-"*25, file=fileOut)
+            print("-"*25, file=fileOut)
             
         
-        return observation, reward, done, info
+        return np.array([0, observation]), reward, done, info
     
     def render(self, mode="human"):
         screen_h = 600
         screen_w = 1000
         if self.viewer is None:
-            self.viewer = rendering.Viewer(screen_w, screen_h)
+            self.viewer = rendering.Viewer(screen_w, screen_h, "SB")
             
         return self.viewer.render(return_rgb_array=mode == "rgb_array")
     
@@ -101,80 +135,138 @@ class SmartBuildingEnv(Env):
             self.viewer.close()
             self.viewer = None
 
-
 # ------------------------------
 #            EV 
 # ------------------------------
-# class ChargingStationEnv(Env):
-#     def __init__(self):
-#         self.viewer = None
-#         self.load = 0
-#         self.required = []
-#         self.timeStep = 0
-#         # self.time = 0
-#         # self.reward = 0
-#         self.action_space = None
-#         self.observation_space = None
-#         self.reset()
+class ChargingStationEnv(Env):
+    def __init__(self):
+        self.viewer = None
+        self.load = []
+        # self.required = []
+        self.required = 2
+        self.timeStep = 0
+        self.totalReward = 0
+        self.chargingDeadline = 2
+        self.demandCharge = 0
+        # self.time = 0
+        # self.reward = 0
+        self.action_space = None
+        self.observation_space = None
+        self.reset()
         
-#     def reset(self):
-#         # self.time = 0
-#         self.timeStep = 0
-#         self.required = [1, 1, 0]
-#         self.load = max(sum([l for l in self.required]), 0)
-#         self.action_space = Box(low=0, high=self.load, shape=(1,), dtype=float)
-#         self.observation_space = Box(low=0, high=self.required[self.timeStep], shape=(1,), dtype=float)
-        
-#         return 0
-#         # pass
-#         # return state
+    def reset(self):
+        # self.time = 0
+        self.timeStep = 0
+        self.totalReward = 0
+        # self.required = [1, 1, 0]
+        self.load = []
+        self.chargingDeadline = 2
+        self.required = 2
+        # self.chargingDeadline = len(self.required) - 1
+        # self.chargingDeadline = len(self.required) - 1
+        # print("Charging deadline: ", self.chargingDeadline, file=fileOut)
+        # self.load = sum([l for l in self.required])
+        self.action_space = Box(low=0, high=self.required - sum([i for i in self.load]), shape=(1,), dtype=float)
+        # INCREASE OBSERVATION SPACE IF TRAIN LOSS CURVE IS UNIMPRESSIVE
+        self.observation_space = Box(low=np.array([0, 0]), high=np.array([self.required, self.chargingDeadline]), shape=(2,), dtype=float)
     
-#     def step(self, action):
-#         info = {}
-#         reward = 0
-#         done: bool = False
-#         self.load = action[0]
         
-#         print("EV Stepping... ", round(action[0], 2))
-#         print("*"*25)
-#         print("*"*25)
-#         print("EV Stepping... ", round(action[0], 2), file=fileOut)
-#         print("*"*25, file=fileOut)
-#         print("*"*25, file=fileOut)
-               
-        
-#         while not done:
-#             print("Timestep: ", self.timeStep)
-#             print("-"*5)
-#             print("Load: ", round(self.load, 2))
-#             print("Required: ", self.required[self.timeStep])
-#             print("Timestep: ", self.timeStep, file=fileOut)
-#             print("-"*5, file=fileOut)
-#             print("Load: ", round(self.load, 2), file=fileOut)
-#             print("Required: ", self.required[self.timeStep], file=fileOut)
-            
-#             observation = self.required[self.timeStep] 
-            
-#             print("-"*25)
-#             print("-"*25, file=fileOut)
-            
-#             # reward -= (self.required) ** 2
-#             reward -= self.load
-#             self.timeStep += 1
-#             done = True if self.timeStep > 2 else done
-            
-        
-#         return observation, reward, done, info
+        return np.array([0,0])
+        # pass
+        # return state
     
-#     def render(self, mode="human"):
-#         screen_h = 600
-#         screen_w = 400
-#         if self.viewer is None:
-#             self.viewer = rendering.Viewer(screen_w, screen_h)
-            
-#         return self.viewer.render(return_rgb_array=mode == "rgb_array")
+    def step(self, action):
+        info = {}
+        reward = 0
+        done: bool = False
+        self.load.append(action[0])
+        # self.load = action[0]
+        self.demandCharge = max(self.load)
+        
+        
+        print("EV stepping: ", round(self.load[self.timeStep], 2))
+        print("*"*25)
+        print("*"*25)
+        print("EV stepping: ", round(self.load[self.timeStep], 2), file=fileOut)
+        print("*"*25, file=fileOut)
+        print("*"*25, file=fileOut)
+        
+        
+        print("Timestep: ", self.timeStep)
+        print("-"*5)
+        # print("Load: ", round(self.load, 2))
+        print("Load: ", [round(i, 2) for i in self.load][self.timeStep])
+        # print("Required: ", self.required[self.timeStep])
+        print("Required: ", self.required)
+        print("Timestep: ", self.timeStep, file=fileOut)
+        print("-"*5, file=fileOut)
+        # print("Load: ", round(self.load, 2), file=fileOut)
+        print("Load: ", [round(i, 2) for i in self.load][self.timeStep], file=fileOut)
+        # print("Required: ", self.required[self.timeStep], file=fileOut)
+        print("Required: ", self.required, file=fileOut)
     
-#     def close(self):
-#         if self.viewer:
-#             self.viewer.close()
-#             self.viewer = None
+            
+        # observation = self.required[self.timeStep] 
+        # observation = self.required - self.load[0]
+        observation = self.required - self.load[self.timeStep]
+        
+        
+        print("Observation: ", round(observation, 2))
+        print("-"*25)
+        print("Observation: ", round(observation, 2), file=fileOut)
+        print("-"*25, file=fileOut)
+
+        
+        # REWARD ONLY NEEDS TO MAKE SURE REQUIRED ENERGY IS CHARGED IN A FLAT ENERGY CHARGE DISTRIBUTION DUE TO PRICE FOR NOW
+        # MEET DEADLINE BY REQUIRED CHARGING AMOUNT (REQUIRED -> TOTAL ENERGY)
+        # CAN STOP AND RESTART MODEL PER 24 HOURS FOR NOW -> CONVOLUTION (SLIDING WINDOW)
+        
+        
+        if self.timeStep == self.chargingDeadline and sum([i for i in self.load]) < self.required:
+            reward = -10
+            
+        elif self.timeStep < self.chargingDeadline:
+            reward -= self.load[self.timeStep]
+            
+        elif self.timeStep == self.chargingDeadline:
+            reward -= self.load[self.timeStep] + 2*(self.demandCharge)
+            
+        #DOES NOT ACTUALLY CONVERGE BASED ON PREVIOUS EPOCH'S REWARD; 
+        #   -> INSTEAD, REWARD IS TOO RANDOM AT BEGINNING OF EACH EPOCH SO THE 
+        #       AGENT NEVER LEARNS THE PROPER POLICY
+            
+        self.timeStep += 1
+        self.totalReward += reward
+        done = True if self.timeStep > 2 else done
+        # done = True if self.timeStep > 2 or sum([i for i in self.load]) == self.required else done
+        
+        print("Total Reward: ", round(self.totalReward, 2))
+        print("-"*25)
+        print("Total Reward: ", round(self.totalReward, 2), file=fileOut)
+        print("-"*25, file=fileOut)
+        print("-"*25, file=fileOut)
+        print("-"*25)
+        print("-"*25)
+        if done:
+            print("DONE: \nLoad: ", [round(i, 2) for i in self.load], "\nTotal Reward: ", round(self.totalReward, 2))
+            print("DONE: \nLoad: ", [round(i, 2) for i in self.load], "\nTotal Reward: ", round(self.totalReward, 2), file=fileOut)
+            print("-"*25)
+            print("-"*25)
+            print("-"*25, file=fileOut)
+            print("-"*25, file=fileOut)
+            
+        
+        return np.array([0, observation]), reward, done, info
+    
+    def render(self, mode="human"):
+        screen_h = 600
+        screen_w = 1000
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(screen_w, screen_h, "EV")
+            
+        return self.viewer.render(return_rgb_array=mode == "rgb_array")
+    
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
--- JJ_cust/custom_env/custom_env/__init__.py
+++ JJ_cust/custom_env/custom_env/__init__.py
@@ -2,9 +2,8 @@ from gym.envs.registration import register
 
 register(id="SmartBuilding_single-v0",
          entry_point="custom_env.envs:SmartBuildingEnv"
-    
 )
 
 register(id="ChargingStation_single-v0",
         entry_point="custom_env.envs:ChargingStationEnv"
-        )
+)
